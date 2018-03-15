148c336f319325dfc8cd81e3727c9f001338d995
vaja1305 <35728453+vaja1305@users.noreply.github.com>
2018-03-15 16:42:28 +0530

    Create revtstr.C

---
+++ revtstr.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include<string.h>
+int main() 
+{
+	char str[30];
+	int i,n;
+	scanf("%s",str);
+	n=strlen(str);
+	for(i=0;i<n;i++)
+	{
+		if(str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u' || str[i]=='A' || str[i]=='E' || str[i]=='I' || str[i]=='O' || str[i]=='U')
+		{
+			str[i]='+';
+		}
+	}
+	for(i=n;i>=0;i--)
+	{
+		if(str[i]!='+')
+		{
+			printf("%c",str[i]);
+		}
+	}
+ 
+	return 0;
+}
