5a94cec6450219608e7b61d9b7af1d8ca7f02661
vaja1305 <35728453+vaja1305@users.noreply.github.com>
2018-03-15 16:10:11 +0530

    Create roman.C

---
+++ roman.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<srting.h>
+ 
+int main(void) 
+{
+	char str[50];
+	scanf("%s",str);
+	if(str[0]=='S' || str[0]=='s')
+	{
+		printf("\nyes");
+	}
+	else
+	{
+		printf("\nno");
+	}
+	return 0;
+}
+ 
